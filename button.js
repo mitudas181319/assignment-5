43b8c2c3b49a55fd958941e7cc22c4e07dbdefd7
mitudas181319 <mitudas181319@gmail.com>
2025-03-02 15:51:08 +0000

    some function added

---
+++ button.js
@@ -0,0 +1,197 @@
+
+// box-5 
+document.getElementById("btn-box5")
+.addEventListener("click",function(event){
+      event.preventDefault()
+  this.disabled=true;
+  let a= document.getElementById("btn-box5");
+  a.className= "opacity-10 bg-[#3752FD] w-22 rounded-xl text-xl font-normal text-white"
+
+  
+     alert("Board updated succesfully") 
+})
+     const decreaseElement = document.getElementById("decreaseNum");
+        let increaseElement = document.getElementById("increaseNum");
+        let button = document.getElementById("btn-box5");
+
+        button.addEventListener("click", function() {
+            let decreaseValue = parseInt(decreaseElement.innerText);
+            let increaseValue = parseInt(increaseElement.innerText);
+
+            decreaseElement.innerText = decreaseValue - 1;
+            increaseElement.innerText = increaseValue + 1;
+        });
+      
+        const text = document.getElementById("text");
+        button.addEventListener("click", function() {
+            text.style.display = "block";
+        });
+
+
+     
+
+//  box-6 
+document.getElementById("btn-box6")
+.addEventListener("click",function(event){
+      event.preventDefault()
+  this.disabled=true;
+  let a= document.getElementById("btn-box6");
+  a.className= "opacity-10 bg-[#3752FD] w-22 rounded-xl text-xl font-normal text-white"
+      
+     alert("Board updated succesfully")  
+     
+})
+
+const decreasesElement = document.getElementById("decreaseNum");
+        let increasesElement = document.getElementById("increaseNum");
+        let buttons = document.getElementById("btn-box6");
+
+        buttons.addEventListener("click", function() {
+            let decreaseValue = parseInt(decreasesElement.innerText);
+            let increaseValue = parseInt(increasesElement.innerText);
+
+            decreaseElement.innerText = decreaseValue - 1;
+            increaseElement.innerText = increaseValue + 1;
+      });
+        
+      
+        const texted = document.getElementById("texted");
+        buttons.addEventListener("click", function() {
+            texted.style.display = "block";
+        });
+      
+
+        
+
+
+
+// box-7 
+
+document.getElementById("btn-box7")
+.addEventListener("click",function(event){
+      event.preventDefault()
+  this.disabled=true;
+  let a= document.getElementById("btn-box7");
+  a.className= "opacity-10 bg-[#3752FD] w-22 rounded-xl text-xl font-normal text-white"
+      
+     alert("Board updated succesfully")  
+     
+})
+
+const decreElement = document.getElementById("decreaseNum");
+        let increElement = document.getElementById("increaseNum");
+        let btn = document.getElementById("btn-box7");
+
+        btn.addEventListener("click", function() {
+            let decreaseValue = parseInt(decreElement.innerText);
+            let increaseValue = parseInt(increElement.innerText);
+
+            decreaseElement.innerText = decreaseValue - 1;
+            increaseElement.innerText = increaseValue + 1;
+        });
+
+        const p3 = document.getElementById("p3");
+        btn.addEventListener("click", function() {
+            p3.style.display = "block";
+        });
+
+// box-8 
+
+document.getElementById("btn-box8")
+.addEventListener("click",function(event){
+      event.preventDefault()
+  this.disabled=true;
+  let a= document.getElementById("btn-box8");
+  a.className= "opacity-10 bg-[#3752FD] w-22 rounded-xl text-xl font-normal text-white"
+      
+     alert("Board updated succesfully")  
+     
+})
+
+const decreeElement = document.getElementById("decreaseNum");
+        let increeElement = document.getElementById("increaseNum");
+        let btns = document.getElementById("btn-box8");
+
+        btns.addEventListener("click", function() {
+            let decreaseValue = parseInt(decreeElement.innerText);
+            let increaseValue = parseInt(increeElement.innerText);
+
+            decreaseElement.innerText = decreaseValue - 1;
+            increaseElement.innerText = increaseValue + 1;
+        });
+
+        const p4 = document.getElementById("p4");
+        btns.addEventListener("click", function() {
+            p4.style.display = "block";
+        });
+
+// box-9 
+
+document.getElementById("btn-box9")
+.addEventListener("click",function(event){
+      event.preventDefault()
+  this.disabled=true;
+  let a= document.getElementById("btn-box9");
+  a.className= "opacity-10 bg-[#3752FD] w-22 rounded-xl text-xl font-normal text-white"
+      
+     alert("Board updated succesfully")  
+     
+})
+
+const decreesElement = document.getElementById("decreaseNum");
+        let increesElement = document.getElementById("increaseNum");
+        let btnss = document.getElementById("btn-box9");
+
+        btnss.addEventListener("click", function() {
+            let decreaseValue = parseInt(decreesElement.innerText);
+            let increaseValue = parseInt(increesElement.innerText);
+
+            decreaseElement.innerText = decreaseValue - 1;
+            increaseElement.innerText = increaseValue + 1;
+        });
+
+        const p5 = document.getElementById("p5");
+        btnss.addEventListener("click", function() {
+            p5.style.display = "block";
+        });
+
+// box-10 
+
+document.getElementById("btn-box10")
+.addEventListener("click",function(event){
+      event.preventDefault()
+  this.disabled=true;
+  let a= document.getElementById("btn-box10");
+  a.className= "opacity-10 bg-[#3752FD] w-22 rounded-xl text-xl font-normal text-white"
+      
+     alert("Board updated succesfully")  
+     
+})
+
+const decreessElement = document.getElementById("decreaseNum");
+        let increessElement = document.getElementById("increaseNum");
+        let btnsss = document.getElementById("btn-box10");
+
+        btnsss.addEventListener("click", function() {
+            let decreaseValue = parseInt(decreessElement.innerText);
+            let increaseValue = parseInt(increessElement.innerText);
+
+            decreaseElement.innerText = decreaseValue - 1;
+            increaseElement.innerText = increaseValue + 1;
+        });
+
+        const p6 = document.getElementById("p6");
+        btnsss.addEventListener("click", function() {
+            p6.style.display = "block";
+        });
+
+      //   clear history 
+      
+      const clear = document.getElementById("clear-btn");
+        const history = document.getElementById("history");
+           
+            clear.addEventListener("click", function() {
+                  history.style.display = "none";
+           
+      })
+      
